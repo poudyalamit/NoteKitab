4b75a75139c8c95b1b71b1eb582714d306dd340f
poudyalamit <poudyalamit20@gmail.com>
2023-03-13 17:09:43 +0545

    backend done following the deletion of note

--- Backend/Routes/notes.js
+++ Backend/Routes/notes.js
@@ -65,4 +65,26 @@ router.put('/updatenote/:id', fetchuser, async (req, res) => {
     }
 })
 
+// ROUTE 4: Delete an existing Note using: DELETE "/api/notes/deletenote". Login required
+
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    try {
+       
+
+        // Find the note to be deleted and delete it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
+
+        //allow deletion only if user owns this note
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json({ "Success": "Note has been deleted" , note:note});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
 module.exports = router;
