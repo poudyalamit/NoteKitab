7f0304d091643c7745bdae21bb0cf9a7266e9a02
poudyalamit <poudyalamit20@gmail.com>
2023-03-21 14:09:08 +0545

    removed errors and added some icons

--- src/Components/NoteItem.js
+++ src/Components/NoteItem.js
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 const NoteItem = (props) => {
     const { note } = props;
     return (
         <div className='col md-3'>
-            <div class="card my-3" >
-                <div class="card-body">
-                    <h5 class="card-title">{note.title}</h5>
-                    <p class="card-text">{note.description}</p>
+            <div className="card my-3" >
+                <div className="card-body">
+                    <div className="d-flex align-items-center">
+                    <h5 className="card-title">{note.title}</h5>
+                <i className='mx-2'><FaTrashAlt/></i>
+                <i className='mx-2'><FaEdit/></i>
+                    </div>
+                    <p className="card-text">{note.description}</p>
                 </div>
             </div>
         </div>
--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -5,7 +5,7 @@ import noteContext from './noteContext';
 const NoteState = (props) => {
     const notesInitial = [
         {
-            "_id": "640cc15c386ddd2c38b26a6d",
+            "_id": "640cc15c3846ddd2c38b26a6d",
             "user": "640c5ae3925490e68aba933b",
             "title": "New Note",
             "description": "Go for it",
@@ -14,7 +14,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "640cc34286f230f8c78b1553",
+            "_id": "640cc34286f2305f8c78b1553",
             "user": "640c5ae3925490e68aba933b",
             "title": "My Title",
             "description": "Let's goo",
@@ -22,7 +22,7 @@ const NoteState = (props) => {
             "date": "2023-03-11T18:06:58.418Z",
             "__v": 0
         }, {
-            "_id": "640cc34286f230f8c78b1553",
+            "_id": "640cc34286f2304f8c78b1553",
             "user": "640c5ae3925490e68aba933b",
             "title": "My Title",
             "description": "Let's goo",
@@ -30,7 +30,7 @@ const NoteState = (props) => {
             "date": "2023-03-11T18:06:58.418Z",
             "__v": 0
         }, {
-            "_id": "640cc34286f230f8c78b1553",
+            "_id": "640cc34286f230f68c78b1553",
             "user": "640c5ae3925490e68aba933b",
             "title": "My Title",
             "description": "Let's goo",
@@ -38,7 +38,7 @@ const NoteState = (props) => {
             "date": "2023-03-11T18:06:58.418Z",
             "__v": 0
         }, {
-            "_id": "640cc34286f230f8c78b1553",
+            "_id": "640cc34286f230f822c78b1553",
             "user": "640c5ae3925490e68aba933b",
             "title": "My Title",
             "description": "Let's goo",
