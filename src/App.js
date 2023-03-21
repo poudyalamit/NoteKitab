8dc5a7d3dfdfd22401b0afb8be559956448de90a
poudyalamit <poudyalamit20@gmail.com>
2023-03-21 14:14:35 +0545

    added alert file to render some alerts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import About from './Components/About';
 import NoteState from './Context/notes/NoteState';
+import Alert from './Components/Alert';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
 
       <Router>
         <Navbar />
+        <Alert message='NoteKitab OP'/>
         <div className="container">
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
